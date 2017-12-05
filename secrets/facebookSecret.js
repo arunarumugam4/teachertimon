ae665a74088a17e959fe2abd4dd63200f533d0e7
cool arun <coolarun477@gmail.com>
2017-12-04 22:41:14 -0800

    fb redirect url changed

--- secrets/facebookSecret.js
+++ secrets/facebookSecret.js
@@ -1,7 +1,7 @@
 // facebook app secret
 let appId = 1251034234996355;
 let appSecret = "479c0719e5c4947d87981bc652c82782";
-let callbackURL= "http://localhost:3000/auth/facebook/callback";
+let callbackURL= "https://teachertimon.herokuapp.com/auth/facebook/callback";
 let profileURL = 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email';
 let profileFields = ['id', 'email', 'name']; // For requesting permissions from Facebook API
 
